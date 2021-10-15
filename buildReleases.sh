243de6e14c071345b060d88359f1302806bcc501
ian <ianopolous@protonmail.com>
2021-10-15 09:46:20 +0100

    make windows bin .exe

--- buildReleases.sh
+++ buildReleases.sh
@@ -2,7 +2,7 @@
 NAME=ipfs-nucleus
 GOOS=linux GOARCH=amd64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
 GOOS=darwin GOARCH=amd64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
-GOOS=windows GOARCH=amd64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
+GOOS=windows GOARCH=amd64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH.exe daemon/ipfs-nucleus.go'
 GOOS=linux GOARCH=arm64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
 GOOS=darwin GOARCH=arm64 bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
 GOOS=linux GOARCH=arm bash -c 'go build -o '$NAME'-$GOOS-$GOARCH daemon/ipfs-nucleus.go'
