9050a8b26276d52ac5af737a67621fb98fce9329
ian <ianopolous@protonmail.com>
2021-11-07 20:36:23 +0000

    fix release script

--- buildReleases.sh
+++ buildReleases.sh
@@ -2,9 +2,9 @@
 NAME=ipfs
 VERSION=v0.1.0
 
-GOOS=linux GOARCH=amd64 bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
-GOOS=darwin GOARCH=amd64 bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
-GOOS=windows GOARCH=amd64 bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME'.exe daemon/ipfs-nucleus.go'
-GOOS=linux GOARCH=arm64 bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
-GOOS=darwin GOARCH=arm64 bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
-GOOS=linux GOARCH=arm bash -c 'go build -o releases/$VERSION/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
+GOOS=linux GOARCH=amd64 bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
+GOOS=darwin GOARCH=amd64 bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
+GOOS=windows GOARCH=amd64 bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME'.exe daemon/ipfs-nucleus.go'
+GOOS=linux GOARCH=arm64 bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
+GOOS=darwin GOARCH=arm64 bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
+GOOS=linux GOARCH=arm bash -c 'go build -o releases/'$VERSION'/$GOOS-$GOARCH/'$NAME' daemon/ipfs-nucleus.go'
